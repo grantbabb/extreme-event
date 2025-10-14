ecdaf72b286686433d2fd00c428889e3b81234a5
grantbabb <sgrantbabb@gmail.com>
2025-10-14 06:12:12 +0000

    connection via sigv4 auth

--- poc/run.sh
+++ poc/run.sh
@@ -3,5 +3,5 @@ export AWS_REGION=us-west-2
 export S3_BUCKET=datasets-in-out
 export S3_PREFIX=input/road-usa/
 export IAM_ROLE_ARN=arn:aws:iam::063299843915:role/service-role/AWSNeptuneNotebookRole-NeptuneNbUser
-export NEPTUNE_ENDPOINT=db-neptune-1.cluster-ro-criq8uemaejw.us-west-2.neptune.amazonaws.com
+export NEPTUNE_ENDPOINT=db-neptune-1.cluster-criq8uemaejw.us-west-2.neptune.amazonaws.com
 python neptune_bulk_load.py
